bab691e0cceab297d8223d6c22c7dfffcdb04908
Gustav Ofosu <gustavofosu@gmail.com>
2022-12-01 09:13:03 -0800

    Updating 2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -12,10 +12,10 @@ if __name__ == "__main__":
     if len_argv > 1:
         print(len_argv, "arguments:")
         for i in range(1, len_argv + 1):
-            print("{:d} {}".format(i, ar[i]))
+            print("{:d}: {}".format(i, ar[i]))
     elif len_argv == 1:
         print(len_argv, "argument:")
         for i in range(1, len_argv + 1):
-            print("{:d} {}".format(i, ar[i]))
+            print("{:d}: {}".format(i, ar[i]))
     elif len_argv == 0:
         print(len_argv, "arguments.")
