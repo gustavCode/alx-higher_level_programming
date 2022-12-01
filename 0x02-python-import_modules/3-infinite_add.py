72fd50258e6a1ef9a96e0269b9157b9d58d08ea3
Gustav Ofosu <gustavofosu@gmail.com>
2022-12-01 09:23:52 -0800

    Adding 3-infinite_add.py

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+from sys
+
+if __name__ = "__main__":
+    """
+    Prints the results of the addition of all arguments
+    """
+
+    ar = sys.argv
+    l_arg = len(ar)
+    sum_arg = 0
+
+    if l_arg > 1:
+        for i in range(1, l_arg):
+            sum += int(ar[i])
+
+    print(sum_arg)
