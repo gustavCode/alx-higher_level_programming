05fda80f569d9b3956537558aa010bcb18c5a3aa
Gustav Ofosu <gustavofosu@gmail.com>
2022-12-01 09:54:55 -0800

    Udating 4-hidden_discovery.py

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -4,6 +4,6 @@ import hidden_4
 if __name__ == "__main__":
     d_names = dir(hidden_4)
 
-    for in in range(len(d_names)):
+    for i in range(len(d_names)):
         if d_names[i][:2] != "__":
             print(d_names[i])
