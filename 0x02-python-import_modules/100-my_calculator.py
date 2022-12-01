1c373f9df6031129e2805bcd30246b5127646a3c
Gustav Ofosu <gustavofosu@gmail.com>
2022-12-01 11:46:29 -0800

    Updating 100-my_calculator.py

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -10,25 +10,25 @@ if __name__ == "__main__":
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
     else:
-        num_1 = int(argv[1])
+        a = int(argv[1])
         operator = argv[2]
-        num_2 = int(argv[3])
+        b = int(argv[3])
 
         if (operator == "+"):
-            result = add(num_1, num_2)
-            print("{:d} + {:d} = {:d}".format(num_1, num_2, result))
+            result = add(a, b)
+            print("{:d} + {:d} = {:d}".format(a, b, result))
             exit(0)
         elif (operator == "-"):
-            result = sub(num_1, num_2)
-            print("{:d} - {:d} = {:d}".format(num_1, num_2, result))
+            result = sub(a, b)
+            print("{:d} - {:d} = {:d}".format(a, b, result))
             exit(0)
         elif (operator == "*"):
-            result = mul(num_1, num_2)
-            print("{:d} * {:d} = {:d}".format(num_1, num_2, result))
+            result = mul(a, b)
+            print("{:d} * {:d} = {:d}".format(a, b, result))
             exit(0)
         elif (operator == "/"):
-            result = div(num_1, num_2)
-            print("{:d} / {:d} = {:d}".format(num_1, num_2, result))
+            result = div(a, b)
+            print("{:d} / {:d} = {:d}".format(a, b, result))
             exit(0)
         else:
             print("Unknown operator. Available operators: +, -, * and /")
