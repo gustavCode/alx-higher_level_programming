2993366852c871b054656636414bf8c0bc16d306
Gustav Ofosu <gustavofosu@gmail.com>
2022-12-01 09:53:33 -0800

    Adding 4-hidden_discovery.py

---
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+import hidden_4
+
+if __name__ == "__main__":
+    d_names = dir(hidden_4)
+
+    for in in range(len(d_names)):
+        if d_names[i][:2] != "__":
+            print(d_names[i])
