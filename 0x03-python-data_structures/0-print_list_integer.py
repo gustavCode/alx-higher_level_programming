a1047bf140f15f2beaa53dbbf2db57399372e51f
Gustav Ofosu <gustavofosu@gmail.com>
2022-12-02 04:18:07 -0800

    Updating 0-print_list_integer.py

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -5,4 +5,4 @@ Prints all integers of a list
 """
 def print_list_integer(my_list=[]):
     for i in range(len(my_list)):
-        print("{:d}}".format(my_list[i]))
+        print("{:d}}".format(i))
