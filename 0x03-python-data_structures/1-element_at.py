c9ecc5e4af4b5053188503ca294a62bb2fe3be36
Gustav Ofosu <gustavofosu@gmail.com>
2022-12-02 03:06:43 -0800

    Adding 1-element_at.py

---
+++ 0x03-python-data_structures/1-element_at.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+"""
+Retrieves an element from a list
+"""
+def element_at(my_list, idx):
+    if (idx < 0):
+        return None
+    elif (idx > len(my_list)):
+        return None
+    else:
+        return my_list[idx]
