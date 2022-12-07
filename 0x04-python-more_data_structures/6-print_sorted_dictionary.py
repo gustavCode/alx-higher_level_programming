690b5e81e3845167992e71a0a38ca87057f11aa2
Gustav Ofosu <gustavofosu@gmail.com>
2022-12-07 10:13:39 -0800

    Updating files

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -3,4 +3,4 @@ def print_sorted_dictionary(a_dictionary):
     s_dictionary = sorted(a_dictionary.items())
 
     for x, y in s_dictionary:
-        print(x, ':', y)
+        print('{0}: {1}'.format(x, y))
