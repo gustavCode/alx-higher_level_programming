df80dc0d4e6eb6a1bf12681569cc08820959c419
Gustav Ofosu <gustavofosu@gmail.com>
2022-12-21 23:54:28 -0800

    Updating files

--- 0x06-python-classes/101-square.py
+++ 0x06-python-classes/101-square.py
@@ -7,20 +7,32 @@ Square class
 
 
 class Square:
-    """Represent a square."""
+    """Square
+
+    Represent a square.
+
+    """
 
     def __init__(self, size=0, position=(0, 0)):
-        """Initialize a new square.
+        """__init__
+
+        Initialize a new square.
+        
         Args:
             size (int): The size of the new square.
             position (int, int): The position of the new square.
-        """
+       
+       """
         self.size = size
         self.position = position
 
     @property
     def size(self):
-        """Get/set the current size of the square."""
+        """size
+
+        Set's the current size of the square.
+
+        """
         return (self.__size)
 
     @size.setter
@@ -33,7 +45,11 @@ class Square:
 
     @property
     def position(self):
-        """Get/set the current position of the square."""
+        """position
+
+        Set's the current position of the square.
+
+        """
         return (self.__position)
 
     @position.setter
@@ -46,11 +62,19 @@ class Square:
         self.__position = value
 
     def area(self):
-        """Return the current area of the square."""
+        """area
+
+        Return the current area of the square.
+
+        """
         return (self.__size * self.__size)
 
     def my_print(self):
-        """Print the square with the # character."""
+        """my_print
+
+        Print the square with the # character.
+
+        """
         if self.__size == 0:
             print("")
             return
@@ -62,7 +86,11 @@ class Square:
             print("")
 
     def __str__(self):
-        """Define the print() representation of a Square."""
+        """__str__
+
+        Define the print() representation of a Square.
+
+        """
         if self.__size != 0:
             [print("") for i in range(0, self.__position[1])]
         for i in range(0, self.__size):
