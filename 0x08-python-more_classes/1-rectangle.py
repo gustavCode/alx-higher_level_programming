fff8e92075da184d5a7a7374d85625fddf96ce9b
Gustav Ofosu <gustavofosu@gmail.com>
2023-01-03 21:29:35 -0800

    Updating files

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -55,7 +55,7 @@ def width(self, value):
 
     """
 
-    if type(value) is not int:
+    if not isinstance(value, int):
         raise TypeError("width must be an integer")
     if value < 0:
         raise ValueError("width must be >= 0")
@@ -87,7 +87,7 @@ def height(self, value):
         ValueError: If `value` is less than `0`.
 
     """
-    if type(value) is not int:
+    if not isinstance(value, int):
         raise TypeError("height must be an integer")
     if value < 0:
         raise ValueError("height must be >= 0")
