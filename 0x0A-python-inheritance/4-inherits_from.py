f27808ccaeeac8b79ff3ea94dfe3302d8fdc0205
Gustav Ofosu <gustavofosu@gmail.com>
2023-01-10 03:24:51 -0800

    Updating files

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -21,6 +21,6 @@ def inherits_from(obj, a_class):
 
     if isinstance(obj, a_class) and \
             issubclass(a_class, obj.__class__) is False:
-                return True
+        return True
 
     return False
