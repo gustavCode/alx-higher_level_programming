32bec73d2639df64d84093f498dc99bfe48e7d80
Gustav Ofosu <gustavofosu@gmail.com>
2023-03-15 01:08:39 +0000

    Updating files

--- 0x13-javascript_objects_scopes_closures/1-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -1,12 +1,8 @@
 #!/usr/bin/node
 
 module.exports = class Rectangle {
-
-  width;
-  height;
-
-  constructor(w, h) {
-    this.width = w;
-    this.height = h;
+  constructor(width, height) {
+    this.width = width;
+    this.height = height;
   }
 }
